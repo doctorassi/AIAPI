ea6b821da3edf89825d875d739c493bf6a5acc29
YaMoma <doctorassi@icloud.com>
2021-12-20 23:14:56 +0100

    1 trying to fix MessageType

--- src/main/java/nl/aipaskie/aiapi/MessageType.java
+++ src/main/java/nl/aipaskie/aiapi/MessageType.java
@@ -7,24 +7,27 @@ import org.bukkit.entity.Player;
 public class MessageType{
 
     public String discordLink = "Test";
+    private AIApi plugin = AIApi.getPlugin(AIApi.class);
 
-    public final void NOTICE(Player p , String s){
+    public void NOTICE(Player p, String s){
         p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "NOTICE" + ChatColor.WHITE + "] " + s);
     }
-    public final void SERVER(Player p , String s) {
-        p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "SERVER " + ChatColor.WHITE + "] " + s);
+    public void SERVER(Player player, String s) {
+        Player p = Bukkit.getPlayer(player.getUniqueId());
+        p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "SERVER " + ChatColor.WHITE + "] " + s);;
     }
-    public final static void WARNING(Player p , String s){
+    public void WARNING(Player p , String s){
         p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "WARNING " + ChatColor.WHITE + "] " + s);
     }
-    public final static void AIPVP(Player p , String s){
+    public  void AIPVP(Player p , String s){
         p.sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "PVP" + ChatColor.WHITE + "] " + s);
     }
-    public final void BROADCAST (String s){
+    public void BROADCAST (String s){
         Bukkit.broadcastMessage(ChatColor.YELLOW + "[" + ChatColor.RED + "SERVER" + ChatColor.YELLOW + "] " + ChatColor.WHITE + s);
     }
-    public void firstJoin(Player p){
-        BROADCAST(ChatColor.AQUA + p.getName() + ChatColor.WHITE + " Has joineed the server for the first time!");
+    public void firstJoin(Player player){
+        Player p = Bukkit.getPlayer(player.getUniqueId());
+        //BROADCAST(ChatColor.AQUA + p.getName() + ChatColor.WHITE + " Has joineed the server for the first time!");
         p.sendMessage("Welcome to the AIPVP server!");
         p.sendMessage("do /intro to do a tour about this network!");
         p.sendMessage("Discord: " + discordLink);
